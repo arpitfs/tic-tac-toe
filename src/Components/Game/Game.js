d377f3cf1934aafd0b33d3cef76845513755ab48
arpitfs <amarpitmalik204@gmail.com>
2020-11-21 00:25:39 +0530

    added state to start the game

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -2,18 +2,34 @@ import React from 'react';
 import Board from '../Board/Board';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStart: false,
+    };
+  }
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
+      if(!this.state.isStart)
+      {
+        return (
+            <button className="enter-button" onClick={() => this.setState({ isStart: true })}>Enter the game</button>
+        );
+      }
+      else
+      {
+        return (
+          <div className="game">
+            <div className="game-board">
+              <Board />
+            </div>
+            <div className="game-info">
+              <div>{/* status */}</div>
+              <ol>{/* TODO */}</ol>
+            </div>
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        );
+      }
+      
     }
   }
 
